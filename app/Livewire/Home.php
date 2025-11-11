3bf12ee62e2ece4ea05445da573beeea37c13684
LEONARDO <leosivi@gmail.com>
2025-11-11 08:44:18 -0400

    Adiciona arquivo database.sqlite

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -8,9 +8,6 @@ class Home extends Component
 {
     public function render()
     {
-        // return view('livewire.home'); //usando </x-app-layout>
-        // return view('livewire.home')->layout('layouts.app'); //usando ->layout('layouts.app')
-
         return view('livewire.home')->layout('layouts.app');
     }
 
